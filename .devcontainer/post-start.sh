ab925bf2e48b76adb4a46e0f99524fdc987b4cf5
D3rrick44 <123391611+D3rrick44@users.noreply.github.com>
2023-09-19 13:16:13 -0400

    Initial commit

---
+++ .devcontainer/post-start.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# this runs each time the container starts
+
+echo "post-start start"
+echo "$(date +'%Y-%m-%d %H:%M:%S')    post-start start" >> "$HOME/status"
+
+echo "Pulling docker images"
+docker pull mcr.microsoft.com/dotnet/sdk:7.0
+docker pull mcr.microsoft.com/dotnet/aspnet:7.0-alpine
+docker pull ghcr.io/cse-labs/res-edge-webv:beta
+docker pull ghcr.io/cse-labs/res-edge-automation:beta
+
+echo "post-start complete"
+echo "$(date +'%Y-%m-%d %H:%M:%S')    post-start complete" >> "$HOME/status"
